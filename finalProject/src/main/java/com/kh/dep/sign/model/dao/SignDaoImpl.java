154ecefe6077bfb4f94e1d5cbc8044144f6d13a9
Feelpark <keakaka@naver.com>
2018-10-16 09:18:10 +0900

    222

--- finalProject/src/main/java/com/kh/dep/sign/model/dao/SignDaoImpl.java
+++ finalProject/src/main/java/com/kh/dep/sign/model/dao/SignDaoImpl.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.kh.dep.sign.model.vo.InsertSign;
 import com.kh.dep.sign.model.vo.MemberDep;
 
 @Repository
@@ -15,6 +16,29 @@ public class SignDaoImpl implements SignDao{
 	public ArrayList<MemberDep> selectMemberDep(SqlSessionTemplate sqlSession, String depName) {
 		
 		return (ArrayList)sqlSession.selectList("sign.selectMemberDep", depName);
+	}
+
+	@Override
+	public int insertDocApproval(SqlSessionTemplate sqlSession, InsertSign is) {
+		return sqlSession.insert("sign.insertDocApproval", is);
+	}
+
+	@Override
+	public int insertApproval_record(SqlSessionTemplate sqlSession, InsertSign is) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int insertReceiving_Check(SqlSessionTemplate sqlSession, InsertSign is) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int insertAttachment(SqlSessionTemplate sqlSession, InsertSign is) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 	
 }
