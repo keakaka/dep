3ec256f0fb2aebcf225eea3cf976a89d529fd716
Shin <Shin@DESKTOP-5SQJTMV>
2018-10-20 13:16:42 +0900

    1020_final_jae 오전

--- finalProject/src/main/java/com/kh/dep/attachment/model/dao/AttachDaoImpl.java
+++ finalProject/src/main/java/com/kh/dep/attachment/model/dao/AttachDaoImpl.java
@@ -1,9 +1,12 @@
 package com.kh.dep.attachment.model.dao;
 
+import java.util.ArrayList;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.kh.dep.attachment.model.vo.Attachment;
+import com.kh.dep.board.model.vo.Board;
 
 @Repository
 public class AttachDaoImpl implements AttachDao {
@@ -13,6 +16,18 @@ public class AttachDaoImpl implements AttachDao {
 		
 		
 		return sqlSession.insert("Member.insertAttach", file);
+	}
+
+	@Override
+	public int insertBoardAttach(SqlSessionTemplate sqlSession, Attachment file) {
+		
+		return sqlSession.insert("Board.insertBoardAttach", file);
+	}
+
+	@Override
+	public ArrayList<Attachment> selectDownloadList(SqlSessionTemplate sqlSession, Board b) {
+		
+		return (ArrayList)sqlSession.selectList("Board.selectDownList", b);
 	}
 
 	
